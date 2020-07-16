e73ab67e1fb045ab20fca2531b97a25dbb54c868
untilthecore <814898862@qq.com>
2020-07-17 01:50:36 +0800

    自定义hook-useUpdate,使函数组件可以忽略初始化那次渲染，实现和类组件componentDidUpdate一样的功能。

--- src/components/functionComponent.js
+++ src/components/functionComponent.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 // 函数组件练习，函数组件如何使用生命周期钩子
+
+// 自定义钩子，实现对数据监听时排除第一次初始化那次
+// 注意：想要在一个函数内部使用hook函数，那这个函数也必须是hook函数或者是自定义hook函数。
+// 而自定义hook函数命名必须要以use开头的命名，这是官方的规定。
+// 具体看文档 ==> https://zh-hans.reactjs.org/docs/hooks-custom.html
+
+
+// 当state被设置的时候，即代码中的setCount，那么就会触发组件重新渲染
+// 所以每次点击使n更新也造成count更新，使得useUpdate被调用两次
+// 解决方法是阻止重新渲染，类组件有shouldComponentUpdate，函数组件又怎么办呢?
+const useUpdate = (fn,num) => {
+    console.log('组件产生更新')
+    const [count,setCount] = useState(0)
+    useEffect( () => {
+        // setCount(x => {
+        //     console.log(x + 1)
+        //    return x + 1 }
+        // )
+    },[])
+    //
+    // useEffect( () => {
+    //     console.log(count);
+    //     if(count >= 1) {
+    //         fn()
+    //         // console.dir(fn)
+    //         // console.log('yes')
+    //     }
+    //     // TODO 数组不监听 fn 会有警告信息，监听了会产生调用两次fn的bug
+    // },[count])
+}
 
 const FunComponent = () => {
     const [n, setN] = React.useState(0)
@@ -30,6 +60,11 @@ const FunComponent = () => {
         console.log('第一次渲染父组件!')
     }, [])
 
+    const fn = () => {
+        console.log('n改变了!')
+    }
+    // 使用自定义hook
+    useUpdate( fn,n)
     return (
         <div>
             <p>我是FunComponent</p>
@@ -52,6 +87,7 @@ const Child = () => {
     // 模拟componentWillUnmount() 钩子函数
     // 注意一定要在第二个参数加 [] ,表示在加载和消亡时只调用一次
     React.useEffect(() => {
+        // 只处理卸载请求就可省略return之外的内容。
         console.log('第一次渲染Child组件!')
         return () => {
             console.log('Child消亡了！')
